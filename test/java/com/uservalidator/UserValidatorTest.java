3ab2a458dbd92480f459d5baf2ea0d84187fb096
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-20 08:02:02 +0530

    Refactored : code for second name entry

--- test/java/com/uservalidator/UserValidatorTest.java
+++ test/java/com/uservalidator/UserValidatorTest.java
@@ -11,4 +11,18 @@ public class UserValidatorTest
     {
         System.out.println(" Welcome to the program");
     }
+    @Test
+    public void given_firstName_WhenProper_GiveTrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean result;
+        result = userValidator.validateFirstName("Kale");
+    }
+    //Checking the validity of second name
+    @Test
+    public void given_LastName_Proper_Give_True()
+    {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validLastName("Ibrah");
+        Assert.assertTrue(result);
+    }
 }
