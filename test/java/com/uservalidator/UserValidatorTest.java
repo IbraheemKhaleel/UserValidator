0b33895ae4ee9eb2666b251c1e62ea60076ea656
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-20 07:51:13 +0530

    Added : Welcome message

---
+++ test/java/com/uservalidator/UserValidatorTest.java
@@ -0,0 +1,14 @@
+package com.uservalidator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserValidatorTest
+{
+    //Welcome text
+    @Test
+    public void Welcome()
+    {
+        System.out.println(" Welcome to the program");
+    }
+}
